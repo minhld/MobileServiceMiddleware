690a805bbce73ec7fd66553ae3861444c003cbad
Le Dinh Minh <ledinhminh3883@gmail.com>
2017-09-07 10:12:44 -0600

    integrating the middleware to the app (on-going)

---
+++ app/src/main/java/com/usu/mobileserviceapp/JobSupporter.java
@@ -0,0 +1,22 @@
+package com.usu.mobileserviceapp;
+
+import android.content.Context;
+
+import com.usu.mobileservice.jobex.DataParser;
+
+/**
+ * Created by minhld on 10/17/2016.
+ */
+
+public class JobSupporter {
+    static DataParser dataParser;
+
+    public static void initDataParser(Context c, String jarPath) throws Exception {
+        // dataParser = new WordDataParserImpl();
+    }
+
+    public static byte[] getData(String filePath) throws Exception {
+        Object dataObj = dataParser.loadObject(filePath);
+        return dataParser.parseObjectToBytes(dataObj);
+    }
+}
--- app/src/main/java/com/usu/mobileserviceapp/MainActivity.java
+++ app/src/main/java/com/usu/mobileserviceapp/MainActivity.java
@@ -1,9 +1,58 @@
 package com.usu.mobileserviceapp;
 
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.usu.mobileservice.utils.Utils;
+
+import butterknife.BindView;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.createGroupBtn)
+    Button createGroupBtn;
+
+    @BindView(R.id.discoverBtn)
+    Button discoverBtn;
+
+    @BindView(R.id.pubBtn)
+    Button pubBtn;
+
+    @BindView(R.id.subBtn)
+    Button subBtn;
+
+    @BindView(R.id.workerBtn)
+    Button workerBtn;
+
+    @BindView(R.id.deviceList)
+    ListView deviceList;
+
+    @BindView(R.id.infoText)
+    TextView infoText;
+
+    @BindView(R.id.imageView)
+    ImageView viewer;
+
+    Handler mainUiHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case Utils.MESSAGE_INFO: {
+                    String strMsg = (String) msg.obj;
+                    UITools.writeLog(MainActivity.this, infoText, strMsg);
+                    break;
+                }
+            }
+        }
+    };
+
+    MidSupporter midSupporter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
