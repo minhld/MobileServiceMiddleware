e67c3028262e359e57ed8f546378aefd9fc3d36c
minhld <ledinhminh3883@gmail.com>
2017-09-24 01:20:33 -0600

    fixed the problem related to javax on annotation projects

--- annotationchecker/src/main/java/com/usu/mobileservice/annotationchecker/MainActivity.java
+++ annotationchecker/src/main/java/com/usu/mobileservice/annotationchecker/MainActivity.java
@@ -3,6 +3,7 @@ package com.usu.mobileservice.annotationchecker;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -10,20 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        int sum = setIt(1, 2, 3, 5, 7);
     }
 
-    private int setIt(int... hellos) {
-        int sum = 0;
-        for (int i = 0; i < hellos.length; i++) {
-            sum += hellos[i];
-        }
-        return sum;
-    }
-
-    // @StatusInfo
-    public class User {
-        String firstName;
-        String lastName;
-        String city;
-    }}
+}
---
+++ annotationchecker/src/main/java/com/usu/mobileservice/annotationchecker/User.java
@@ -0,0 +1,13 @@
+package com.usu.mobileservice.annotationchecker;
+
+import com.usu.servicemiddleware.annotations.StatusInfo;
+
+/**
+ * Created by lee on 9/23/17.
+ */
+@StatusInfo
+public class User {
+    String firstName;
+    String lastName;
+    String city;
+}
