1b7ffaaa13716a65d5ce4318de01785482c769f1
Le Dinh Minh <ledinhminh3883@gmail.com>
2017-09-27 12:59:50 -0600

    re-added the original mobile service back to the project

---
+++ servicemiddleware/src/main/java/com/usu/mobileservice/jobimpls/EmptyDataParserImpl.java
@@ -0,0 +1,64 @@
+package com.usu.mobileservice.jobimpls;
+
+import com.usu.mobileservice.utils.Utils;
+
+/**
+ *
+ * Created by minhld on 01/28/2016.
+ */
+public class EmptyDataParserImpl implements com.usu.mobileservice.jobex.DataParser {
+
+    @Override
+    public Class getDataClass() {
+        return String.class;
+    }
+
+    @Override
+    public Object loadObject(String path) throws Exception {
+        return "please wait!";
+    }
+
+    @Override
+    public Object parseBytesToObject(byte[] byteData) throws Exception {
+        return Utils.deserialize(byteData);
+    }
+
+    @Override
+    public byte[] parseObjectToBytes(Object objData) throws Exception {
+        if (objData instanceof byte[]) {
+            return (byte[]) objData;
+        } else {
+            return Utils.serialize(objData);
+        }
+    }
+
+    @Override
+    public byte[] getPartFromObject(Object data, int firstOffset, int lastOffset) {
+        try {
+            return parseObjectToBytes(data);
+        } catch(Exception e) {
+            return new byte[0];
+        }
+    }
+
+    @Override
+    public Object createPlaceHolder(Object dataObject) {
+        return "placeholder:";
+    }
+
+    @Override
+    public Object copyPartToHolder(Object placeholderObj, byte[] partObj, int firstOffset, int lastOffset) {
+        String placeholderPath = (String) placeholderObj;
+        placeholderPath = placeholderPath + partObj;
+        return placeholderPath;
+    }
+
+    @Override
+    public void destroyObject(Object data) {}
+
+    @Override
+    public boolean isObjectDestroyed(Object data) {
+        return true;
+    }
+
+}
---
+++ servicemiddleware/src/main/java/com/usu/mobileservice/jobimpls/EmptyJobImpl.java
@@ -0,0 +1,24 @@
+package com.usu.mobileservice.jobimpls;
+
+import com.usu.mobileservice.jobex.Job;
+import com.usu.mobileservice.utils.Utils;
+
+/**
+ * Created by minhld on 12/10/2016.
+ */
+public class EmptyJobImpl implements Job {
+
+	public Object exec(Object input) {
+        // this will do nothing but waiting for very long - WAITING_TIME
+        // int partNum = (Integer) input;
+        // int partWaitTime = WAITING_TIME / partNum;
+        try {
+            Thread.sleep(Utils.WAITING_TIME);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return "finished waiting!";
+    }
+}
