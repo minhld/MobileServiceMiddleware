fd34262c74de050b5ea6230d14b96a6f004e3c7a
Le Dinh Minh <ledinhminh3883@gmail.com>
2017-10-13 18:42:32 -0600

    some minor updates

--- annotationchecker/src/main/java/com/usu/mobileservice/annotationchecker/User.java
+++ annotationchecker/src/main/java/com/usu/mobileservice/annotationchecker/User.java
@@ -1,13 +1,15 @@
 package com.usu.mobileservice.annotationchecker;
 
 import com.usu.servicemiddleware.annotations.MobileService;
+import com.usu.servicemiddleware.annotations.ServiceMethod;
 
 /**
  * Created by lee on 9/23/17.
  */
 @MobileService
 public class User {
-    String firstName;
-    String lastName;
-    String city;
+    @ServiceMethod
+    public String[] getFolderList(String root) {
+        return new String[] { "hello", "there" };
+    }
 }
