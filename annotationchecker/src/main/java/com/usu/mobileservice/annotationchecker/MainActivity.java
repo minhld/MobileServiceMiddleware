a913b7ef3af31b6d4f4e0114dc718a1f31d7052f
minhld <ledinhminh3883@gmail.com>
2017-10-27 00:37:25 -0600

    removed redundant code

--- annotationchecker/src/main/java/com/usu/mobileservice/annotationchecker/MainActivity.java
+++ annotationchecker/src/main/java/com/usu/mobileservice/annotationchecker/MainActivity.java
@@ -4,7 +4,6 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.pm.PackageManager;
 import android.os.Build;
-import android.os.Environment;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
